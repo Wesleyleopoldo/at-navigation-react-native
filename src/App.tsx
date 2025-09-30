b25dd1e82119e325a3a04d066f590b9d6a2a597f
Your Name <you@example.com>
2025-09-30 20:43:37 -0300

    feat: Feito tab e adicionado icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
@@ -7,35 +7,29 @@ import Sobre from './pages/Sobre';
 import { options } from './styles';
 import Contato from './pages/Contato';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} 
-        options={{
-          title: options.title,
-          headerStyle: options.headerStyle,
-          headerTitleStyle: options.headerTitleStyle,
-          headerShown: false
-        }}
-        />
-        <Stack.Screen name="Sobre" component={Sobre} 
-        options={{
-          title: options.title,
-          headerStyle: options.headerStyle,
-          headerTitleStyle: options.headerTitleStyle,
-          headerShown: true,
-        }}
+      <Tab.Navigator>
+        
+        <Tab.Screen
+        name='Home'
+        component={Home}
         />
 
-        <Stack.Screen
+        <Tab.Screen
+        name='Sobre'
+        component={Sobre}
+        />
+
+        <Tab.Screen
         name='Contato'
         component={Contato}
         />
 
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
