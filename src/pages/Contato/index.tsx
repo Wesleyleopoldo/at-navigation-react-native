e3e8bad5e7cd57455cec208b8dab761795ab8882
Your Name <you@example.com>
2025-09-30 20:00:04 -0300

    feat: Adiciona botão de retornar para a Home

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -1,14 +1,21 @@
-import { View, Text } from "react-native";
-
+import { View, Text, Button } from "react-native";
+import { useNavigation, StackActions } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../types/types";
 
-type ContatoScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, "Home">;
+type ContatoScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, "Contato">;
 
 export default function Contato() {
+    const navigation = useNavigation<ContatoScreenNavigationProp>();
+
+    function handleHome() {
+        navigation.dispatch(StackActions.popToTop());
+    }
+
     return (
         <View>
             <Text>Página de contato do app.</Text>
+            <Button title="Voltar para a Home" onPress={handleHome} />
         </View>
     )
 }
